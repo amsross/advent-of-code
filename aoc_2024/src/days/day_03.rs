77847c3e8403f431d136d6dbbea135fb7aba437e
Matt Ross <amsross@gmail.com>
2024-12-23 14:01:39 -0500

    feat: complete day 3 part 2

--- aoc_2024/src/days/day_03.rs
+++ aoc_2024/src/days/day_03.rs
@@ -15,6 +15,32 @@ pub fn part_01(contents: String) -> i32 {
         .fold(0, |acc, (left, right)| acc + left * right)
 }
 
+// https://adventofcode.com/2024/day/3#part2
+#[allow(dead_code)]
+pub fn part_02(contents: String) -> i32 {
+    let re = Regex::new(r"(mul\([0-9]+,[0-9]+\))|do\(\)|don't\(\)").unwrap();
+    let matches = re
+        .find_iter(&contents)
+        .map(|m| m.as_str())
+        .collect::<Vec<&str>>();
+
+    let mut apply = true;
+
+    matches.iter().fold(0, |acc, m| {
+        match m {
+            &"do()" => apply = true,
+            &"don't()" => apply = false,
+            m => {
+                if apply {
+                    return acc + part_01(m.to_owned().to_string());
+                }
+            }
+        }
+
+        acc
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -39,5 +65,26 @@ mod tests {
         let result = part_01(contents);
 
         assert_eq!(result, 170778545);
+    }
+
+    #[test]
+    fn test_part_02_01() {
+        // https://adventofcode.com/2024/day/3#part2
+        let contents: String =
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))".to_owned();
+
+        let result = part_02(contents);
+
+        assert_eq!(result, 48);
+    }
+
+    #[test]
+    fn test_part_02_02() {
+        // https://adventofcode.com/2024/day/3/input
+        let contents = fs::read_to_string("./input/day_03_2.txt").expect("unable to read file");
+
+        let result = part_02(contents);
+
+        assert_eq!(result, 82868252);
     }
 }
