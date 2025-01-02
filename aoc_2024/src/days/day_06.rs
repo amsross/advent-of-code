8167118637e04afca9345e60adda136c91c53766
Matt Ross <amsross@gmail.com>
2025-01-02 18:14:59 -0500

    feat: complete day 6 part 1

--- aoc_2024/src/days/day_06.rs
+++ aoc_2024/src/days/day_06.rs
@@ -1,12 +1,70 @@
 use crate::helpers::string_to_matrix;
+use std::collections::HashSet;
+
+#[allow(dead_code)]
+fn turn(direction: char) -> char {
+    match direction {
+        '^' => '>',
+        '>' => 'v',
+        'v' => '<',
+        '<' => '^',
+        _ => panic!("Unable to turn: Invalid direction"),
+    }
+}
+
+#[allow(dead_code)]
+fn coords_are_within_bounds(matrix: &Vec<Vec<char>>, (x, y): (i32, i32)) -> bool {
+    x >= 0 && y >= 0 && x < matrix[0].len() as i32 && y < matrix.len() as i32
+}
+
+#[allow(dead_code)]
+fn move_forward((x, y): (i32, i32), direction: char) -> (i32, i32) {
+    match direction {
+        '^' => (x, y - 1),
+        '>' => (x + 1, y),
+        'v' => (x, y + 1),
+        '<' => (x - 1, y),
+        _ => panic!("Unable to move: Invalid direction"),
+    }
+}
 
 // https://adventofcode.com/2024/day/6
 #[allow(dead_code)]
 pub fn part_01(contents: String) -> i32 {
-    let _matrix = string_to_matrix(contents);
-    println!("{:?}", _matrix);
+    let matrix = string_to_matrix(contents);
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
 
-    0
+    // step through each coord in the matrix
+    for row in 0..matrix.len() {
+        for col in 0..matrix[row].len() {
+            let mut x = col as i32;
+            let mut y = row as i32;
+            let mut direction = matrix[row][col];
+
+            // if we find a starting point
+            if direction == '^' {
+                loop {
+                    visited.insert((x, y));
+                    let (new_x, new_y) = move_forward((x, y), direction);
+
+                    if coords_are_within_bounds(&matrix, (new_x, new_y)) {
+                        if matrix[new_y as usize][new_x as usize] == '#' {
+                            direction = turn(direction);
+                        } else {
+                            x = new_x;
+                            y = new_y;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+
+                return visited.len() as i32;
+            }
+        }
+    }
+
+    visited.len() as i32
 }
 
 // https://adventofcode.com/2024/day/6#part2
@@ -45,14 +103,13 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part_01_02() {
         // https://adventofcode.com/2024/day/6/input
         let contents = fs::read_to_string("./input/day_06_1.txt").expect("unable to read file");
 
         let result = part_01(contents);
 
-        assert_eq!(result, 2358);
+        assert_eq!(result, 4663);
     }
 
     #[test]
