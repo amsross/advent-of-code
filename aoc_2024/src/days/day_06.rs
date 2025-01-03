d10a7a150f9617d78a07cf6f93e6a9dc67070079
Matt Ross <amsross@gmail.com>
2025-01-02 22:11:30 -0500

    feat: start day 6 part 2 (DNF)

--- aoc_2024/src/days/day_06.rs
+++ aoc_2024/src/days/day_06.rs
@@ -1,5 +1,5 @@
 use crate::helpers::string_to_matrix;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 #[allow(dead_code)]
 fn turn(direction: char) -> char {
@@ -59,7 +59,7 @@ pub fn part_01(contents: String) -> i32 {
                     }
                 }
 
-                return visited.len() as i32;
+                break;
             }
         }
     }
@@ -70,9 +70,57 @@ pub fn part_01(contents: String) -> i32 {
 // https://adventofcode.com/2024/day/6#part2
 #[allow(dead_code)]
 pub fn part_02(contents: String) -> i32 {
-    let _matrix = string_to_matrix(contents);
+    let matrix = string_to_matrix(contents);
+    let mut turns: HashSet<(i32, i32)> = HashSet::new();
+    let mut steps: HashSet<(i32, i32)> = HashSet::new();
+    let mut obstacles: HashMap<char, HashSet<(i32, i32)>> = HashMap::from([
+        ('^', HashSet::new()),
+        ('>', HashSet::new()),
+        ('v', HashSet::new()),
+        ('<', HashSet::new()),
+    ]);
 
-    0
+    // step through each coord in the matrix
+    for row in 0..matrix.len() {
+        for col in 0..matrix[row].len() {
+            let mut x = col as i32;
+            let mut y = row as i32;
+            let mut direction = matrix[row][col];
+
+            // if we find a starting point
+            if direction == '^' {
+                loop {
+                    let (new_x, new_y) = move_forward((x, y), direction);
+
+                    if coords_are_within_bounds(&matrix, (new_x, new_y)) {
+                        if matrix[new_y as usize][new_x as usize] == '#' {
+                            turns.insert((x, y));
+                            obstacles.get_mut(&direction).unwrap().insert((x, y));
+                            direction = turn(direction);
+                        } else {
+                            steps.insert((x, y));
+                            x = new_x;
+                            y = new_y;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+
+                break;
+            }
+        }
+    }
+
+    for step in steps.clone().into_iter() {
+        println!("step: ({}, {})", step.0, step.1);
+    }
+
+    for (direction, _coords) in obstacles.into_iter() {
+        println!("direction: {:#?}", direction);
+    }
+
+    steps.len() as i32
 }
 
 #[cfg(test)]
@@ -116,11 +164,23 @@ mod tests {
     #[ignore]
     fn test_part_02_01() {
         // https://adventofcode.com/2024/day/4#part2
-        let contents: String = "".to_owned();
+        let contents: String = "
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#..."
+            .trim()
+            .to_owned();
 
         let result = part_02(contents);
 
-        assert_eq!(result, 9);
+        assert_eq!(result, 6);
     }
 
     #[test]
@@ -131,6 +191,6 @@ mod tests {
 
         let result = part_02(contents);
 
-        assert_eq!(result, 1737);
+        assert_eq!(result, 0);
     }
 }
